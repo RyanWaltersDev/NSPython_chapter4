1bd292ae7b3806de8a8778df59657953ae68709f
Ryan Walters <RyanWaltersDev@gmail.com>
2020-12-03 18:07:00 -0500

    +initial README +first ch4 project

---
+++ README.txt
@@ -0,0 +1,1 @@
+Collection of all the projects in Chapter 4 of the No Starch Python book
---
+++ magicians.py
@@ -0,0 +1,13 @@
+#Ryan Walters Dec3 2020 -- A program to demonstrate looping with lists
+
+#Initial list and first for loop
+magicians = ['copperfield', 'randi', 'blaine']
+for magician in magicians:
+	print(magician)
+
+#Doing more with the loop
+for magician in magicians:
+	print(f"Wow, {magician.title()}! Your magic trick is astonishing!")
+	print(f"I am certain that your next trick will be even better, {magician.title()}!\n")
+	
+#END OF PROGRAM
