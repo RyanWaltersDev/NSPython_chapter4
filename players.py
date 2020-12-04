b877fccff6f44a8d0a60ff4ea6c511f7c036c791
Ryan Walters <RyanWaltersDev@gmail.com>
2020-12-04 16:37:35 -0500

    +finish numberical +two new projects

---
+++ players.py
@@ -0,0 +1,9 @@
+#Ryan Walters Dec4 2020 -- Practicing slicing a list
+
+#Initial list and slicing
+players = ['mike', 'dave', 'zac', 'adam', 'april']
+print(players[0:3])
+print(players[2:5])
+print(players[1:4])
+print(players[:2])
+print(players[3:])
